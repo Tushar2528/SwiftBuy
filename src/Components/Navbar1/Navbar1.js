d30081aa8d33bf073c7b03469d65eaab2395c82b
Tushar Bahuguna <tusharbahuguna2528@gmail.com>
2023-07-25 20:27:36 +0530

    Remove consoles from other componenta

--- src/Components/Navbar1/Navbar1.js
+++ src/Components/Navbar1/Navbar1.js
@@ -1,10 +1,10 @@
 import nav1Styles from "./Navbar1.module.css";
-import navStyles from "../Navbar/Navbar.module.css";
+
 import cart from "../../Images/carts.png";
 import home from "../../Images/home.png";
 import order from "../../Images/checklist.png";
 import logout from "../../Images/switch.png";
-import { NavLink , Link, useNavigate} from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 import { useParams } from "react-router-dom";
 
 
@@ -35,35 +35,32 @@ function Navbar1() {
                 <div >
                     SwiftBuy
                 </div>
-                {/* <div className={navStyles.rightSide}>
-                    <div>Home</div>
-                    <Link className={navStyles.nav} to="sign-in"><div >SignIn</div></Link>
-                </div> */}
+               
                 <div className={nav1Styles.rightSide}>
                     
                         <div>
-                            <div onClick={gotoHome} className={nav1Styles.imageDiv}><img src={home}></img></div>
+                            <div onClick={gotoHome} className={nav1Styles.imageDiv}><img src={home} alt=""></img></div>
                             <div  onClick={gotoHome} className={nav1Styles.nameDiv}>Home</div>
                         </div>
                     
 
                     
                         <div>
-                            <div className={nav1Styles.imageDiv} onClick={gotoOrders}><img src={order}></img></div>
+                            <div className={nav1Styles.imageDiv} onClick={gotoOrders}><img src={order} alt=""></img></div>
                             <div className={nav1Styles.nameDiv} onClick={gotoOrders}>My Orders</div>
                         </div>
                     
 
                     
                         <div>
-                            <div className={nav1Styles.imageDiv} onClick={gotoCart}><img src={cart}></img></div>
+                            <div className={nav1Styles.imageDiv} onClick={gotoCart}><img src={cart} alt=""></img></div>
                             <div className={nav1Styles.nameDiv} onClick={gotoCart}>Cart</div>
                         </div>
                     
 
                     
                         <div>
-                            <div onClick={gotoHome} className={nav1Styles.imageDiv}><img src={logout}></img></div>
+                            <div onClick={gotoHome} className={nav1Styles.imageDiv}><img src={logout} alt=""></img></div>
                             <div onClick={gotoHome} className={nav1Styles.nameDiv}>Logout</div>
                         </div>
                     
